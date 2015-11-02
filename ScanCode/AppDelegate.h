c2b6f9329323a29907dd428202e78bb066310938
Tolecen <taoxinle@imac.local>
2015-11-02 16:35:08 +0800

    Initial Commit

---
+++ ScanCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanCode
+//
+//  Created by TaoXinle on 15/11/2.
+//  Copyright © 2015年 TaoXinle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScanCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanCode
+//
+//  Created by TaoXinle on 15/11/2.
+//  Copyright © 2015年 TaoXinle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
